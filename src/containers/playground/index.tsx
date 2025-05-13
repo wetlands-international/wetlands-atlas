a8f705eb970a7d26c097691e1729c4f80d2497b1
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-05-13 08:42:30 +0200

    Gradients and postprocessing

--- src/containers/playground/index.tsx
+++ src/containers/playground/index.tsx
@@ -1,19 +1,52 @@
 "use client";
-
-import { OrbitControls } from "@react-three/drei";
+import {
+  GradientTexture,
+  GradientType,
+  OrbitControls,
+  OrthographicCamera,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { EffectComposer, DotScreen } from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import { useWindowSize } from "usehooks-ts";
 
 export const Playground = () => {
+  const { width, height } = useWindowSize();
   return (
-    <div className="grid min-h-[calc(100svh_-_theme(spacing.16))] items-center justify-items-center">
-      <Canvas linear>
-        <ambientLight intensity={0.5} />
+    <div className="grid min-h-[100svh] items-center justify-items-center">
+      <Canvas>
+        <ambientLight intensity={1} />
         <directionalLight position={[1, 2, 5]} />
 
-        <points>
-          <torusKnotGeometry args={[1, 0.4, 100, 16]} />
-          <pointsMaterial size={0.05} color="orange" />
-        </points>
+        <mesh>
+          <circleGeometry args={[4, 64, 0]} />
+          <meshStandardMaterial>
+            <GradientTexture
+              stops={[0, 0.3, 0.35, 0.65, 0.67, 1]} // As many stops as you want
+              colors={["aquamarine", "aquamarine", "hotpink", "hotpink", "yellow", "yellow"]} // Colors need to match the number of stops
+              size={1024} // Size (height) is optional, default = 1024
+              width={1024} // Width of the canvas producing the texture, default = 16
+              type={GradientType.Radial} // The type of the gradient, default = GradientType.Linear
+              innerCircleRadius={0} // Optional, the radius of the inner circle of the gradient, default = 0
+              outerCircleRadius={"auto"} // Optional, the radius of the outer circle of the gradient, default = auto
+            />
+          </meshStandardMaterial>
+        </mesh>
+        <EffectComposer>
+          <DotScreen blendFunction={BlendFunction.MULTIPLY} scale={1} opacity={1} />
+        </EffectComposer>
+
+        <OrthographicCamera
+          makeDefault
+          zoom={100}
+          top={height / 2}
+          bottom={-height / 2}
+          left={width / 2}
+          right={-width / 2}
+          near={1}
+          far={2000}
+          position={[0, 0, -200]}
+        />
 
         <OrbitControls />
       </Canvas>
