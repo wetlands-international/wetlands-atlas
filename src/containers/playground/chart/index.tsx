45d9f4943172d6f882106e6f321d9a51b9b42151
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-05-18 23:42:08 +0200

    Discs gradient

--- src/containers/playground/chart/index.tsx
+++ src/containers/playground/chart/index.tsx
@@ -1,11 +1,10 @@
-import { Fragment, useMemo, useState } from "react";
+import { Fragment, useMemo } from "react";
 
+import { Disc } from "@/containers/playground/chart/disc";
 import { Ring } from "@/containers/playground/chart/ring";
 import { Wave } from "@/containers/playground/chart/wave";
 
 export const RingChart = () => {
-  const [click, setClick] = useState(0);
-
   const DATA = useMemo(() => {
     // Generate random values that sum 1 on every click
     const randomValues = Array.from({ length: 3 }, () => Math.random());
@@ -13,7 +12,7 @@ export const RingChart = () => {
     const normalizedValues = randomValues.map((value) => value / sum);
     const colors = ["#4287f5", "#77bbe6", "#77d9e6"];
     const newData = normalizedValues.map((value, index) => ({
-      id: index + click,
+      id: index,
       name: `Ring ${index + 1}`,
       color: colors[index],
       value,
@@ -27,7 +26,7 @@ export const RingChart = () => {
     //   { id: 2, name: "Ring 2", color: "#77bbe6", value: 0.5, speed: 1 },
     //   { id: 3, name: "Ring 3", color: "#77d9e6", value: 0.25, speed: 1.5 },
     // ];
-  }, [click]);
+  }, []);
 
   return (
     <group>
@@ -58,16 +57,7 @@ export const RingChart = () => {
           </Fragment>
         );
       })}
-      <mesh onClick={() => setClick((prev) => prev + 1)}>
-        <circleGeometry args={[1, 64]} scale={[1, 1, 0.01]} />
-        <meshStandardMaterial
-          color="#000000"
-          transparent
-          opacity={0.25}
-          depthWrite={false}
-          depthTest={false}
-        />
-      </mesh>
+      <Disc />
     </group>
   );
 };
