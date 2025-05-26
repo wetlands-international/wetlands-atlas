8ca86b9a0534b213aa6d322f188b05725773fcb4
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-05-26 22:31:06 +0200

    Image: effect

--- app/src/containers/playground/image/fragment.glsl
+++ app/src/containers/playground/image/fragment.glsl
@@ -6,7 +6,8 @@ uniform float uRevealThreshold;
 
 varying vec2 vUv;
 
-#include "lygia/generative/curl.glsl";
+#include "lygia/generative/voronoise.glsl";
+
 
 void main() {
   vec4 heightTexture = texture2D(uHeightmap, vUv);
@@ -15,14 +16,23 @@ void main() {
   float progress = uRevealThreshold;
   // progress = clamp(progress, 0.0, 1.0);
 
+  // reveal the image from the center towards the edges, they should end up with an alpha of 1.0
+  // Compute radial distance from center
+  vec2 center = vec2(0.5, 0.5); // Image center in UV space
+  float dist = distance(vUv, center);
+
+  // Use smoothstep to create a soft radial reveal
+  float radialAlpha = smoothstep(progress - 0.1, progress + 0.1, dist);
+
   float revealAlpha = smoothstep(heightTexture.r, 1.0, progress);
 
-  vec3 noise = curl(vec3(vUv, progress));
-  vec2 displacedUv = vUv + sin(noise.xy * 0.05); // Adjust the scale of the noise as needed
+  float noise = voronoise(vUv, progress, progress);
+  vec2 displacedUv = vUv + vec2(sin(noise * 0.05)); // Adjust the scale of the noise as needed
   vec4 displacedColor = texture2D(uImage, displacedUv);
   colorTexture.rgb = mix(displacedColor.rgb, colorTexture.rgb, revealAlpha);
-  // colorTexture.rgb = mix(colorTexture.rgb, displacedColor.rgb, sin(uTime) * 0.5 + 0.5); // Use sine wave for smooth transition
 
+
+  
   // add a frame around the image to indicate the reveal
-  gl_FragColor = vec4(colorTexture.rgb, revealAlpha);
+  gl_FragColor = vec4(colorTexture.rgb, (1.0 - radialAlpha) * revealAlpha);
 }
