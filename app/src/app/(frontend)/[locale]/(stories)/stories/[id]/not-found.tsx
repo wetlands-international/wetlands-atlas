3063d23617d353436d60caad76f675aede1d1112
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-06-06 10:07:09 +0200

    Style fixes

--- app/src/app/(frontend)/[locale]/(stories)/stories/[id]/not-found.tsx
+++ app/src/app/(frontend)/[locale]/(stories)/stories/[id]/not-found.tsx
@@ -2,26 +2,32 @@ import { useTranslations } from "next-intl";
 
 import { Header } from "@/containers/header";
 
+import { Button } from "@/components/ui/button";
+
 import { Link } from "@/i18n/navigation";
 
 export default function NotFoundPage() {
   const t = useTranslations("storiesId.not-found");
 
   return (
-    <>
+    <aside className="z-10 w-xl shrink-0">
       <Header className="sticky top-0 z-20 p-4" blur={false}>
         <div className="flex items-center gap-2">
           <Link href="/stories">Stories</Link>
         </div>
       </Header>
-      <aside className="px-11 py-10">
+      <article className="space-y-5 px-11 py-10">
         <div className="prose prose-invert">
           <h1 className="font-display animate-in fade-in slide-in-from-top-10 text-7xl font-bold text-blue-300">
             {t("title")}
           </h1>
           <p className="text-lg">{t("description")}</p>
         </div>
-      </aside>
-    </>
+
+        <Link href="/stories">
+          <Button>{t("back")}</Button>
+        </Link>
+      </article>
+    </aside>
   );
 }
