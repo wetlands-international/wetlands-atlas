db9343bf6e63108da0e68d5cfbf1c55de0f8dcf9
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-06-18 09:26:06 +0200

    Indicators: url management

--- app/src/containers/indicators/item.tsx
+++ app/src/containers/indicators/item.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useCallback } from "react";
+
+import { useSyncIndicators } from "@/app/(frontend)/[locale]/(app)/store";
+
 import RankingChartComponent from "@/components/chart/ranking";
 // import ScatterChartComponent from "@/components/chart/scatter";
 import { Lexical } from "@/components/ui/lexical";
@@ -8,11 +12,23 @@ import { Switch } from "@/components/ui/switch";
 import { Indicator } from "@/payload-types";
 
 export const IndicatorsItem = (indicator: Indicator) => {
+  const [indicators, setIndicators] = useSyncIndicators();
+
+  const handleSwitchChange = useCallback(() => {
+    setIndicators((prev) => {
+      if (prev?.includes(indicator.id)) {
+        return prev.filter((id) => id !== indicator.id);
+      } else {
+        return [...(prev || []), indicator.id];
+      }
+    });
+  }, [indicator.id, setIndicators]);
+
   return (
     <div key={indicator.id} className="bg-background rounded-4xl p-6">
       <header className="flex items-center justify-between gap-2">
         <h2 className="text-base font-semibold uppercase">{indicator.name}</h2>
-        <Switch />
+        <Switch checked={indicators?.includes(indicator.id)} onCheckedChange={handleSwitchChange} />
       </header>
 
       {!!indicator.description && (
