a8233f79c2ca59d0d6008a5b44489fbdc80e5835
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-06-20 10:47:34 +0200

    Insights: category back button

--- app/src/containers/categories/back.tsx
+++ app/src/containers/categories/back.tsx
@@ -2,36 +2,46 @@
 
 import { useCallback } from "react";
 
-import { LuChevronRight } from "react-icons/lu";
+import { PaginatedDocs } from "payload";
+
+import { LuX } from "react-icons/lu";
 
 import { cn } from "@/lib/utils";
 
-import { useSyncInsight } from "@/app/(frontend)/[locale]/(app)/store";
+import { useSyncIndicators, useSyncInsight } from "@/app/(frontend)/[locale]/(app)/store";
 
 import { Category } from "@/payload-types";
 
-export const CategoriesItem = (category: Category) => {
-  const [, setInsight] = useSyncInsight();
+export type CategoriesBackProps = {
+  categories?: PaginatedDocs<Category>;
+};
 
-  const handleChange = useCallback(() => {
-    setInsight(category.id);
-  }, [category.id, setInsight]);
+export const CategoriesBack = ({ categories }: CategoriesBackProps) => {
+  const [insight, setInsight] = useSyncInsight();
+  const [, setIndicators] = useSyncIndicators();
+
+  const category = categories?.docs.find((c) => c.id === insight);
+
+  const handleClick = useCallback(() => {
+    setInsight(null);
+    setIndicators(null);
+  }, [setInsight, setIndicators]);
 
   return (
     <button
-      key={category.id}
+      key={category?.id}
       className={cn(
         "group bg-background relative cursor-pointer rounded-4xl bg-cover bg-position-[50%_50%] text-left transition-all duration-500 hover:bg-position-[50%_45%]",
         {
-          "pointer-events-none grayscale": !category.indicators?.docs?.length,
+          "pointer-events-none grayscale": !category?.indicators?.docs?.length,
         },
       )}
       style={{
-        backgroundImage: `url(https://picsum.photos/seed/g-${category.id}/500/300)`,
+        backgroundImage: `url(https://picsum.photos/seed/g-${category?.id}/500/300)`,
       }}
-      onClick={handleChange}
+      onClick={handleClick}
     >
-      {!category.indicators?.docs?.length && (
+      {!category?.indicators?.docs?.length && (
         <span className="text-foreground bg-foreground/20 text-2xs pointer-events-none absolute top-3 left-4 z-10 rounded-4xl px-2.5 py-1.5 font-medium uppercase backdrop-blur-xs">
           Coming soon
         </span>
@@ -44,24 +54,24 @@ export const CategoriesItem = (category: Category) => {
       >
         <header className="relative flex min-h-28 w-full flex-col justify-center">
           <div className="relative w-full">
-            <h2 className="font-display text-2xl">{category.name}</h2>
-            {/* {!!category.description && (
+            <h2 className="font-display text-2xl">{category?.name}</h2>
+            {!!category?.description && (
               <div
                 className={cn(
                   "prose prose-invert prose-sm w-full duration-300",
                   // "-translate-y-2 opacity-0 duration-500 group-hover:translate-y-0 group-hover:opacity-100",
                 )}
               >
-                {category.description}
+                {category?.description}
               </div>
-            )} */}
+            )}
           </div>
         </header>
 
-        {!!category.indicators?.docs?.length && (
+        {!!category?.indicators?.docs?.length && (
           <div className="bg-background/50 group-hover:bg-primary relative flex size-12 shrink-0 items-center justify-center rounded-full backdrop-blur-xs transition-colors duration-500">
             <span className="bg-primary absolute top-1/2 left-1/2 hidden h-9/12 w-9/12 -translate-1/2 rounded-full group-hover:block group-hover:animate-ping" />
-            <LuChevronRight className="text-foreground relative z-10 size-5" />
+            <LuX className="text-foreground relative z-10 size-5" />
           </div>
         )}
       </div>
