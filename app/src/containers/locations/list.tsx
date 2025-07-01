20f4c822dd13c823a0015207e3e9fdfb3a3b6a1b
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-07-02 00:09:10 +0200

    Improve filtering behaviour

--- app/src/containers/locations/list.tsx
+++ app/src/containers/locations/list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSuspenseQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { CommandEmpty, CommandGroup, CommandList } from "cmdk";
 import { useAtomValue } from "jotai";
 import { useLocale } from "next-intl";
@@ -16,12 +16,14 @@ export const LocationsList = () => {
   const locale = useLocale();
   const locations = useAtomValue(locationsAtom);
 
-  console.log("LocationsAtom", locations);
-
   const [search] = useDebounceValue(locations.search, 300);
 
-  const { data: locationsData } = useSuspenseQuery(
-    API.queryOptions("get", "/api/indicators", {
+  const {
+    data: locationsData,
+    isFetched,
+    isFetching,
+  } = useQuery({
+    ...API.queryOptions("get", "/api/indicators", {
       params: {
         query: {
           depth: 1,
@@ -39,32 +41,38 @@ export const LocationsList = () => {
         },
       },
     }),
-  );
+    placeholderData: keepPreviousData,
+  });
 
   return (
     <CommandList className="bg-foreground rounded-4xl p-6">
-      <CommandEmpty>
-        <span className="text-muted-foreground">No locations found</span>
-      </CommandEmpty>
-      <CommandGroup
-        heading="All regions"
-        className="[&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group-heading]]:text-bold [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:uppercase"
-      >
-        <div className="py-2">
-          <LocationsItem>Worldwide</LocationsItem>
-          <LocationsItem>Location</LocationsItem>
-          <LocationsItem>Location 2</LocationsItem>
-          <LocationsItem>Location 3</LocationsItem>
-          <LocationsItem>Location 4</LocationsItem>
-          <LocationsItem>Location 5</LocationsItem>
-          <LocationsItem>Location 6</LocationsItem>
-          <LocationsItem>Location 7</LocationsItem>
+      {!isFetching && isFetched && (
+        <CommandEmpty>
+          <span className="text-muted-foreground">No locations found</span>
+        </CommandEmpty>
+      )}
 
-          {locationsData?.docs.map((indicator) => (
-            <LocationsItem key={indicator.id}>{indicator.name}</LocationsItem>
-          ))}
-        </div>
-      </CommandGroup>
+      {!!locationsData?.docs.length && (
+        <CommandGroup
+          heading="All regions"
+          className="[&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group-heading]]:text-bold [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:uppercase"
+        >
+          <div className="py-2">
+            {/* <LocationsItem>Worldwide</LocationsItem>
+            <LocationsItem>Location</LocationsItem>
+            <LocationsItem>Location 2</LocationsItem>
+            <LocationsItem>Location 3</LocationsItem>
+            <LocationsItem>Location 4</LocationsItem>
+            <LocationsItem>Location 5</LocationsItem>
+            <LocationsItem>Location 6</LocationsItem>
+            <LocationsItem>Location 7</LocationsItem> */}
+
+            {locationsData?.docs.map((indicator) => (
+              <LocationsItem key={indicator.id}>{indicator.name}</LocationsItem>
+            ))}
+          </div>
+        </CommandGroup>
+      )}
     </CommandList>
   );
 };
