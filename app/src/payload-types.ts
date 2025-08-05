471d66d9ab78b52d3e7d13056e1dcc22a7be5b5c
Alejandro Peralta <alepefe@protonmail.com>
2025-08-05 10:21:53 +0200

    feat!: Integrate Google Cloud Storage for media management

--- app/src/payload-types.ts
+++ app/src/payload-types.ts
@@ -157,6 +157,7 @@ export interface User {
 export interface Media {
   id: number;
   alt: string;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -502,6 +503,7 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
