e812203f868503e52887c47f19e9bfa19ea3611c
Alejandro Peralta <alepefe@protonmail.com>
2025-08-06 13:51:15 +0200

    fix!: Enable GCS plugin when env.NODE_ENV === "production"

--- app/src/cms/plugins/index.ts
+++ app/src/cms/plugins/index.ts
@@ -12,7 +12,7 @@ export const plugins: Plugin[] = [
   openapi({ openapiVersion: "3.1", metadata: { title: "Dev API", version: "0.0.1" } }),
 ];
 
-if (env.NODE_ENV !== "production") {
+if (env.NODE_ENV === "production") {
   plugins.push(
     gcsPrefixPlugin({
       projectId: env.GCS_PROJECT_ID,
