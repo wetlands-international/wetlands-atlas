e62e7cc03663ae5a17dc76edb18d565988a0b392
Alejandro Peralta <alepefe@protonmail.com>
2025-09-02 11:54:10 +0200

    feat(tiler): Add TileOutsideBounds error handler

--- tiler/main.py
+++ tiler/main.py
@@ -1,11 +1,11 @@
 import os
-import random
-import json
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from titiler.core.factory import TilerFactory
-from titiler.core.dependencies import ColorMapParams
 from starlette.middleware.cors import CORSMiddleware
+from fastapi import Request
+from fastapi.responses import JSONResponse
+from rio_tiler.errors import TileOutsideBounds
 
 load_dotenv()
 
@@ -24,6 +24,16 @@ app.add_middleware(
 cog_tiler = TilerFactory()
 
 app.include_router(cog_tiler.router, tags=["tiles"])
+
+# Exception handler for tile errors
+@app.exception_handler(TileOutsideBounds)
+def tile_outside_bounds_handler(request: Request, exc: TileOutsideBounds):
+    return JSONResponse(
+        status_code=404,
+        content={
+            "detail": f"Tile is outside bounds: {exc}"
+        },
+    )
 
 @app.get("/healthz")
 def health():
