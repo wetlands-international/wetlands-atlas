0231ffc73d18807618ec8d09c8696905a66b7512
atrincas <adam.trincas@vizzuality.com>
2025-09-02 11:25:07 +0200

    fe: fix colors RankingChartComponent

--- app/src/components/chart/ranking/index.tsx
+++ app/src/components/chart/ranking/index.tsx
@@ -22,7 +22,7 @@ export default function RankingChartComponent({ data }: { data: IndicatorChartDa
             <li key={index} className="flex flex-col gap-2">
               <div className="flex items-center justify-between gap-2 text-base font-normal">
                 <span className="flex-1">{item.label}</span>
-                <span className="text-blue-300">{formatNumber(item.value)}</span>
+                <span style={{ color: item.color }}>{formatNumber(item.value)}</span>
               </div>
               <div className="h-2.5 w-full rounded-lg bg-[rgba(250,250,250,0.05)]">
                 <div
@@ -30,7 +30,7 @@ export default function RankingChartComponent({ data }: { data: IndicatorChartDa
                     "h-full rounded-l-lg bg-blue-300": true,
                     "rounded-r-lg": scale(item.value) === 100,
                   })}
-                  style={{ width: `${scale(item.value)}%` }}
+                  style={{ width: `${scale(item.value)}%`, backgroundColor: item.color }}
                 ></div>
               </div>
             </li>
