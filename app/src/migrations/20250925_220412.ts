c11f715f515d32fd59593c021e96bae36540961d
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2025-09-26 10:08:22 +0200

    Fix migration

--- app/src/migrations/20250925_220412.ts
+++ app/src/migrations/20250925_220412.ts
@@ -1,13 +1,13 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+import { MigrateUpArgs, MigrateDownArgs, sql } from "@payloadcms/db-postgres";
 
 export async function up({ db, payload, req }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
-   ALTER TABLE "categories" ADD COLUMN "order" numeric NOT NULL;
-  ALTER TABLE "indicators" ADD COLUMN "order" numeric NOT NULL;`)
+   ALTER TABLE "categories" ADD COLUMN "order" numeric;
+  ALTER TABLE "indicators" ADD COLUMN "order" numeric;`);
 }
 
 export async function down({ db, payload, req }: MigrateDownArgs): Promise<void> {
   await db.execute(sql`
    ALTER TABLE "categories" DROP COLUMN "order";
-  ALTER TABLE "indicators" DROP COLUMN "order";`)
+  ALTER TABLE "indicators" DROP COLUMN "order";`);
 }
