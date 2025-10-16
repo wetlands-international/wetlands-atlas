0bb3712ecc1fe8582a836cdd0aa20268632d8ce9
Adam Trincas <atrincas@gmail.com>
2025-10-16 11:28:28 +0200

    cms: fix layer validation (#118)

--- app/src/cms/utils/layer-validation.ts
+++ app/src/cms/utils/layer-validation.ts
@@ -19,7 +19,8 @@ export const LayerTypeValidation: CollectionBeforeValidateHook<Layer> = (args) =
       ],
     });
   }
-  if (data && data.type === LAYER_TYPE.CONTEXTUAL && data.indicators) {
+  console.log(data);
+  if (data && data.type === LAYER_TYPE.CONTEXTUAL && data.indicators?.length) {
     throw new ValidationError({
       collection: "layers",
       errors: [
